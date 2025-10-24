bc7289c4951e00bfa18aba2ba87fb7be8838a025
Vikas Sharma <vikas.sharma0b@gmail.com>
2025-10-24 20:37:17 +0530

    deploy

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+console.log(" BASE_URL :",BASE_URL);
+export const axiosInstance = axios.create({
+  baseURL:BASE_URL+"/api",
+  withCredentials: true,
+});
